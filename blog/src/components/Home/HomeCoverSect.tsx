b30ac254bf91e9df922d731683a923ff0fcf8062
Houdini Papi <ochiengcliff.co@gmail.com>
2024-06-16 00:30:17 +0300

    Tags and elements

--- blog/src/components/Home/HomeCoverSect.tsx
+++ blog/src/components/Home/HomeCoverSect.tsx
@@ -1,6 +1,8 @@
 import { sortBlogs } from '@/utils';
 import React from 'react'
 import Image from "next/image"
+import Link from 'next/link';
+import Tag from '../Elements/tag';
 
 interface Props {
   blogs: any[];
@@ -15,17 +17,36 @@ const HomeCoverSect = ({blogs}: Props) => {
     return (
         <div className='w-full inline-block'>
             <article className='flex flex-col items-start justify-end mx-10 relative h-[85vh]'>
+                <div
+                    className="absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl z-0"
+                />
                 <Image
                     src={blog.image.filePath.replace("../public", "")}
                     placeholder='blur'
                     blurDataURL={blog.image.blurhashDataUrl}
                     alt={blog.title}
                     fill
-                    className='w-full h-full object-center object-cover rounded-3xl'
+                    className='w-full h-full object-center object-cover rounded-3xl -z-10'
                 />
+
+                <div className="w-3/4 p-16 flex flex-col items-start justify-center z-0 text-light">
+                    {/* <Link href={`/categories/${blog.tags[0]}`}>
+                        {blog.tags[0]}
+                    </Link> */}
+                    <Tag
+                        link={`/categories/${blog.tags[0]}`}
+                        name={blog.tags[0]}
+                    />
+                    <h1>
+                        {blog.title}
+                    </h1>
+                    <p>
+                        {blog.description}
+                    </p>
+                </div>
             </article>
         </div>
     )
 }
 
-export default HomeCoverSect
+export default HomeCoverSect;
