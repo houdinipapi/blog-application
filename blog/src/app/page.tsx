e657a1b870d807afc1fe492b205f6b6347e98258
Houdini Papi <ochiengcliff.co@gmail.com>
2024-06-15 21:18:46 +0300

    chore: Import allBlogs from contentlayer.generated in page.tsx

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,6 +1,9 @@
-
+import { allBlogs } from "../../.contentlayer/generated";
 
 export default function Home() {
+
+  console.log(allBlogs);
+  
   return (
     <main className="flex flex-col items-center justify-center">
       <div>
