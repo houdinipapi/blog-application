58a06d1ff08196c0eda70c290c39b795d9d37c03
Houdini Papi <ochiengcliff.co@gmail.com>
2024-06-16 15:30:25 +0300

    feat: Add FirstBlogLayout component for blog display

--- blog/src/components/Home/FeaturedPosts.tsx
+++ blog/src/components/Home/FeaturedPosts.tsx
@@ -1,5 +1,6 @@
 import { sortBlogs } from '@/utils';
 import React from 'react'
+import FirstBlogLayout from '../BlogLayouts/FirstBlogLayout';
 
 interface Props {
   blogs: any[];
@@ -14,6 +15,23 @@ const FeaturedPosts = ({blogs}: Props) => {
         className='w-full mt-32 px-32 flex flex-col items-center justify-center'
     >
         <h2 className='w-full inline-block font-bold capitalize text-4xl'>Featured Posts</h2>
+
+        <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-16">
+
+          <article className='col-span-1 row-span-2 relative'>
+            <FirstBlogLayout blog={sortedBlogs[2]} />
+          </article>
+
+          <article className='col-span-1 row-span-1 relative'>
+            blog layout 2
+          </article>
+
+          <article className='col-span-1 row-span-1 relative'>
+            blog layout 3
+          </article>
+
+        </div>
+
     </section>
   )
 }
